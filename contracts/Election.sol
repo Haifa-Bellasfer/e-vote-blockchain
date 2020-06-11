24dd5591530be37c1ace2be388473b58840c9221
haifa.bellasfer <haifa.bellasfer@esprit.tn>
2020-06-12 00:18:59 +0200

    my comit

---
+++ contracts/Election.sol
@@ -0,0 +1,48 @@
+pragma solidity ^0.5.0 ;
+
+//import "usingOraclize.sol";
+
+contract Election {
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+}
+
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+    // Store Candidates w looking for
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
+    uint public candidatesCount;
+
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
+    constructor() public {
+        addCandidate("Kais Said");
+        addCandidate("Nabil Karoui");
+    }
+
+    function addCandidate(string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+     function vote (uint _candidateId) public {
+        // require that they haven't voted before
+            require (!voters[msg.sender]);
+
+        // require a valid candidate
+            require (_candidateId > 0 && _candidateId <= candidatesCount);
+            
+        // record 
+        voters[msg.sender] = true;
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // declenched event
+        emit votedEvent(_candidateId);
+    }
+}
